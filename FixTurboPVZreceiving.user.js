1fa5532feb9c1603c986d4b580a6161bbe8ce130
KenSteelyHunter <31893777+antonusachenko@users.noreply.github.com>
2023-05-04 20:41:20 +0700

    upload

---
+++ FixTurboPVZreceiving.user.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         Fix for stupid TurboPVZ receiving
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  This script gives the convenience of work, which cannot be given by OZON with a capital of 1 billion rubles
+// @author       Usachenko Antonuy
+// @match        https://pvz.ozon-dostavka.ru/receiving
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ozon-dostavka.ru
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Constans
+    const element = document.getElementsByClassName("logItemList_mw6zT");
+    // const config = { attributes: true, childList: true, subtree: true };
+    // regex for find number
+	const regex = /'\d*\d-\d\d*'/;
+    var superWindow;
+    var superWindowText;
+	//
+    function DrawSuperWindow(){
+        //Draw new element SuperWindow
+        superWindow = document.createElement("div");
+        superWindow.id = "superNumber";
+        superWindow.style.cssText = 'z-index:9999999999;position:fixed;display:table;align-items:center;top:calc(50% - 200px);left:100px;width:550px;height:400px;border-radius: 30px;-moz-border-radius:30px;background-color: #f2f2f2; Display: block;box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, .2);';
+        //superWindow.style.cssText = 'Display: none';
+        document.body.appendChild(superWindow);
+        superWindowText = document.createElement("p");
+        superWindow.appendChild(superWindowText);
+        superWindowText.style.cssText = 'font-size: 20vh;text-align: center;line-height: 400px;';
+        superWindowText.innerHTML = "00-0";
+    }
+
+    // Get element for parsing
+    function updateResult() {
+		var text = element[0].firstChild.lastChild.innerHTML;
+        var result = text.match(regex)[0];
+        //alert(result); //debug
+        // Refresh superWindow
+        //superWindow.style.cssText = 'z-index:9999999999;position:fixed;display:table;align-items:center;top:calc(50% - 200px);left:100px;width:550px;height:400px;border-radius: 30px;-moz-border-radius:30px;background-color: #f2f2f2; Display: block;box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, .2);';
+        superWindowText.innerHTML = result;
+    }
+    // Dw
+    //
+    console.log("Plugin ready to work");
+    //
+    // Create Observer
+    var observer = new MutationObserver(updateResult);
+    console.log("Observer created");
+    //
+    // Add observer if element exist
+    function addObserverIfDesiredNodeAvailable() {
+        var elementNode = document.querySelectorAll(".logItemList_mw6zT")[0];
+        if(elementNode) {
+            var config = {childList: true};
+            // Start observing the target node for configured mutations
+            observer.observe(elementNode, config);
+            console.log("Observer started");
+            DrawSuperWindow();
+            //add listener for close button
+            var closeButtonNode = document.getElementsByClassName("el-drawer__close-btn")[0];
+            closeButtonNode.addEventListener("click", function() {
+                superWindow.style.cssText = 'Display: none';
+            });
+            var openButtonNode = document.getElementsByClassName("el-button--primary")[1];
+            openButtonNode.addEventListener("click", function() {
+                superWindow.style.cssText = 'z-index:9999999999;position:fixed;display:table;align-items:center;top:calc(50% - 200px);left:100px;width:550px;height:400px;border-radius: 30px;-moz-border-radius:30px;background-color: #f2f2f2; Display: block;box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, .2);';
+            });
+        }
+        else{
+            //The node we need does not exist yet.
+            //Wait 500ms and try again
+            window.setTimeout(addObserverIfDesiredNodeAvailable,500);
+            console.log("Element not found, restart the function AddObserverIfDesuredNodeAvailable");
+        }
+
+    }
+    addObserverIfDesiredNodeAvailable();
+
+})();
