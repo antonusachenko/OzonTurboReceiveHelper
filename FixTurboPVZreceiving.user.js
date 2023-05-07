8f86093e945f5a9ebf4fdb12cfdd38a7ee826cee
KenSteelyHunter <31893777+antonusachenko@users.noreply.github.com>
2023-05-07 10:41:33 +0700

    Update 0.2. Fix output format and add speech

--- FixTurboPVZreceiving.user.js
+++ FixTurboPVZreceiving.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Fix for stupid TurboPVZ receiving
+// @name         Fix TurboPVZ receiving
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  This script gives the convenience of work, which cannot be given by OZON with a capital of 1 billion rubles
-// @author       Usachenko Antony
+// @author       Usachenko Antonuy
 // @match        https://pvz.ozon-dostavka.ru/receiving
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ozon-dostavka.ru
 // @grant        none
@@ -15,7 +15,10 @@
     const element = document.getElementsByClassName("logItemList_mw6zT");
     // const config = { attributes: true, childList: true, subtree: true };
     // regex for find number
-	const regex = /'\d*\d-\d\d*'/;
+	const regexUsusal = /'\d*\d-\d\d*'/;
+    const regexCUR = /CUR-\d*/;
+    const regexMixed = /'\d*\d-\d\d*'|CUR-\d*/;
+    const regexResult = /(?<=\').+?(?=\')/;
     var superWindow;
     var superWindowText;
 	//
@@ -31,22 +34,31 @@
         superWindowText.style.cssText = 'font-size: 20vh;text-align: center;line-height: 400px;';
         superWindowText.innerHTML = "00-0";
     }
-
-    // Get element for parsing
-    function updateResult() {
+    //
+    function Speak(text) {
+        const message = new SpeechSynthesisUtterance();
+        message.lang = "ru-RU";
+        message.text = text;
+        window.speechSynthesis.speak(message)
+    }
+    //
+    function UpdateResult() {
 		var text = element[0].firstChild.lastChild.innerHTML;
-        var result = text.match(regex)[0];
+        var match = text.match(regexMixed)[0];
         //alert(result); //debug
         // Refresh superWindow
         //superWindow.style.cssText = 'z-index:9999999999;position:fixed;display:table;align-items:center;top:calc(50% - 200px);left:100px;width:550px;height:400px;border-radius: 30px;-moz-border-radius:30px;background-color: #f2f2f2; Display: block;box-shadow: 12px 12px 2px 1px rgba(0, 0, 255, .2);';
+        var result = match.match(regexResult)[0];
         superWindowText.innerHTML = result;
+        Speak(result);
     }
-    // Dw
+    //
+    //
     //
     console.log("Plugin ready to work");
     //
     // Create Observer
-    var observer = new MutationObserver(updateResult);
+    var observer = new MutationObserver(UpdateResult);
     console.log("Observer created");
     //
     // Add observer if element exist
