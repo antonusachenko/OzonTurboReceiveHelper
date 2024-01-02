46db7aae707a41091d51c87d2be6fdcf571d4c33
KenSteelyHunter <31893777+antonusachenko@users.noreply.github.com>
2024-01-02 14:43:43 +0700

    back to 0.7.0

--- FixTurboPVZreceiving.user.js
+++ FixTurboPVZreceiving.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fix TurboPVZ receiving
 // @namespace    http://tampermonkey.net/
-// @version      0.7.3
+// @version      0.7.0
 // @description  This script provides the convenience of work, which OZON cannot provide with a capital of more than 400 billion rubles.
 // @author       Usachenko Antony
 // @match        https://pvz.ozon-dostavka.ru/receiving/receive
@@ -16,7 +16,7 @@
     const receiveForkClassName = ".logs_kiV3I";
 
     // regex for find number
-    const regexMixed = /\d{1,5}-\d{1,3}|CUR\/\d+|\-\d+\/\d+/g;
+    const regexMixed = /\d{1,5}-\d{1,3}|CUR|\-\d+\/\d+/g;
 
 
     // fields
@@ -111,15 +111,13 @@
                     //
                     Speak(textResult);
                 }
-                if(match == match.match(/CUR\/\d+/)){
+                if(match == match.match(/CUR/)){
                     // Refresh superWindow
                     console.log("Refresh superWindow and speech text");
                     let result = match;
-                    let text2 = result.match(/[^\/]*$/);
                     superWindowText.innerHTML = result;
                     prevMatch = match;
-                    textResult = "Курьерская номер" + text2;
-                    Speak(textResult);
+                    Speak("Курьерская доставка");
                 }
                 if(match == match.match(/\-\d+\/\d+/)){
                     // Refresh superWindow
